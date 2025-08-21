676108d47dfc7b3637057a2c5efb2cab2cc7493e
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2025-08-21 21:24:50 +0200

    fix: flake maybe

--- flake.nix
+++ flake.nix
@@ -5,31 +5,27 @@
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    systems.url = "github:nix-systems/x86_64-linux";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
-    nix-filter.url = "github:numtide/nix-filter";
+    filter.url = "github:numtide/nix-filter";
   };
   outputs = {
-    flake-utils,
     nixpkgs,
     self,
+    filter,
     ...
-  } @ inputs:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        filter = inputs.nix-filter.lib;
-      in {
-        packages = rec {
-          default = qs-config;
-          qs-config = pkgs.callPackage ./pkg.nix {inherit filter;};
-        };
-        homeManagerModules = {
-          qs-config = import ./module.nix self;
-        };
-      }
-    );
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = rec {
+        default = qs-config;
+        qs-config = pkgs.callPackage ./pkg.nix {inherit filter;};
+      };
+      homeManagerModules = {
+        qs-config = import ./module.nix self;
+      };
+    });
+  };
 }
