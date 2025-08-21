0d3a8c1d938b708800fa90441ce3d434e77c791e
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2025-08-21 21:27:53 +0200

    fix: 2

--- flake.nix
+++ flake.nix
@@ -16,16 +16,16 @@
     systems = ["x86_64-linux" "aarch64-linux"];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    packages = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages = rec {
+    packages = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
         default = qs-config;
         qs-config = pkgs.callPackage ./pkg.nix {inherit filter;};
-      };
-      homeManagerModules = {
-        qs-config = import ./module.nix self;
-      };
-    });
+      }
+    );
+    homeManagerModules = {
+      qs-config = import ./module.nix self;
+    };
   };
 }
